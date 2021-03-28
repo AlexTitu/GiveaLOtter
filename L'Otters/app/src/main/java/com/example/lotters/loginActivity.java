6a7c75783809cb5743c250326badcbf99b1d870e
IlfyPlorious <sdragosflorian@gmail.com>
2021-03-28 11:48:20 +0300

    update1

--- L'Otters/app/src/main/java/com/example/lotters/loginActivity.java
+++ L'Otters/app/src/main/java/com/example/lotters/loginActivity.java
@@ -2,13 +2,34 @@ package com.example.lotters;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class loginActivity extends AppCompatActivity {
+
+    private Button loginButton, registerButton;
+    private EditText loginText, passwordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.loginButton);
+        registerButton = findViewById(R.id.registerButton);
+        loginText = findViewById(R.id.username);
+        passwordText = findViewById(R.id.password);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( getApplicationContext(), registerActivity.class);
+                startActivity( intent );
+            }
+        });
+
     }
 }
