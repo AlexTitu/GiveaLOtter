054136b3bdeb12b7abc85fbc55dfb10e9b83b16c
IlfyPlorious <sdragosflorian@gmail.com>
2021-03-28 12:29:37 +0300

    Firebase setup

--- L'Otters/app/src/main/java/com/example/lotters/registerActivity.java
+++ L'Otters/app/src/main/java/com/example/lotters/registerActivity.java
@@ -3,12 +3,20 @@ package com.example.lotters;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class registerActivity extends AppCompatActivity {
+
+    private EditText numeRegister, passwordRegister, emailRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        numeRegister = findViewById(R.id.nume);
+        passwordRegister = findViewById(R.id.registerPassword);
+        emailRegister = findViewById(R.id.email);
+
     }
 }
